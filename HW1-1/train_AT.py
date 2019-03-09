2d5916857c70e1b2fdef5a2d8c85c5dda2a5a330
b05611038 <b05611038@ntu.edu.tw>
2019-03-09 15:43:35 +0800

    edit hsitory file and training process

--- HW1-1/train_AT.py
+++ HW1-1/train_AT.py
@@ -16,7 +16,7 @@ from lib.model import ANN_cifar10, CNN
 #train_AT is to test the performance of different deep learning model whcih has 
 #same amount of parameters but different depth
 #--------------------------------------------------------------------------------
-def TrainModel(model, saving_name, criterion, epochs, device, save = True):
+def TrainModel(model, saving_name, criterion, epochs, batch_size, device, save = True):
     if device < 0:
         env = torch.device('cpu')
     else:
@@ -33,15 +33,17 @@ def TrainModel(model, saving_name, criterion, epochs, device, save = True):
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     train_set = torchvision.datasets.CIFAR10(root = './data', train = True, download = True, transform = transform)
+    train_eval_set = torchvision.datasets.CIFAR10(root = './data', train = True, download = True, transform = transform)
     test_set = torchvision.datasets.CIFAR10(root = './data', train = False, download = True, transform = transform)
 
-    train_loader = DataLoader(train_set, batch_size = 100, shuffle = False)
-    test_loader = DataLoader(test_set, batch_size = 100, shuffle = False)
+    train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = False)
+    train_eval_loader = DataLoader(train_eval_set, batch_size = batch_size, shuffle = False)
+    test_loader = DataLoader(test_set, batch_size = batch_size, shuffle = False)
 
     print('Model Structure:')
     print(model)
 
-    history = ['Epoch,Loss,Acc.\n']
+    history = ['Epoch,Train Loss,Train Acc.,Test Loss,Test Acc.\n']
     for epoch in range(epochs):
         print('Start training epoch: ', epoch + 1)
         for iter, data in enumerate(train_loader):
@@ -58,13 +60,31 @@ def TrainModel(model, saving_name, criterion, epochs, device, save = True):
 
             optim.step()
 
-            if iter != 0 and iter % 2000 == 0:
-                print('Iter: ', iter, '| Loss: %6f' % loss)
+            if iter != 0 and iter % 10 == 0:
+                print('Iter: ', iter, ' | Loss: %6f' % loss.detach())
 
-        test_loss = 0
-        test_count = 0
-        correct = 0
-        total = 0
+        train_loss = []
+        train_total = []
+        train_correct = 0
+        with torch.no_grad():
+            for _iter, train_data in enumerate(train_eval_loader):
+                train_x, train_y = train_data
+                train_x = train_x.float().to(env)
+                train_y = train_y.long().to(env)
+
+                train_out = model(train_x)
+                _, prediction = torch.max(train_out.data, 1)
+                train_total.append(train_y.size(0))
+                train_correct += (prediction == train_y).sum().item()
+
+                train_loss.append(criterion(train_out, train_y).detach())
+
+        train_loss = torch.tensor(train_loss)
+        train_total = torch.tensor(train_total)
+
+        test_loss = []
+        test_total = []
+        test_correct = 0
         with torch.no_grad():
             for _iter, test_data in enumerate(test_loader):
                 test_x, test_y = test_data
@@ -73,15 +93,23 @@ def TrainModel(model, saving_name, criterion, epochs, device, save = True):
 
                 test_out = model(test_x)
                 _, prediction = torch.max(test_out.data, 1)
-                total += test_y.size(0)
-                correct += (prediction == test_y).sum().item()
+                test_total.append(test_y.size(0))
+                test_correct += (prediction == test_y).sum().item()
 
-                test_loss += criterion(test_out, test_y)
-                test_count += 1
+                test_loss.append(criterion(test_out, test_y).detach())
 
-            test_loss = test_loss / test_count
-            history.append(str(epoch + 1) + ',' + str(test_loss) + ',' + str(100 * correct / total) + '\n')
-            print('\nEpoch: ', epoch + 1, '| Testing loss: %6f' % test_loss, '| Testing Acc.: %2f' % (100 * correct / total), '\n')
+        test_loss = torch.tensor(test_loss)
+        test_total = torch.tensor(test_total)
+
+        train_loss = (torch.sum(torch.mul(train_loss, train_total.float())) / torch.sum(train_total)).detach()
+        train_acc = (100 * train_correct / torch.sum(train_total)).detach()
+        test_loss = (torch.sum(torch.mul(test_loss, test_total.float())) / torch.sum(test_total)).detach()
+        test_acc = (100 * test_correct / torch.sum(test_total)).detach()
+
+        history.append(str(epoch + 1) + ',' + str(train_loss) + ',' + str(train_acc) + ','
+                + str(test_loss) + ',' + str(test_acc) + '\n')
+        print('\nEpoch: ', epoch + 1, '| Train loss: %6f' % train_loss, '| Train Acc.: %2f' % train_acc,
+                '| Test loss: %6f' % test_loss, '| Test Acc.: %2f' % test_acc, '\n')
 
     f = open(saving_name + '.csv', 'w')
     f.writelines(history)
@@ -93,8 +121,8 @@ def TrainModel(model, saving_name, criterion, epochs, device, save = True):
     print('All process done.')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 6:
-        print('Usage: python3 train_AT.py [CNN or ANN] [model depth] [model unit] [model name] [epochs] [device]')
+    if len(sys.argv) < 7:
+        print('Usage: python3 train_AT.py [CNN or ANN] [model depth] [model unit] [model name] [epochs] [batch size] [device]')
         exit(0)
 
     start_time = time.time()
@@ -106,7 +134,7 @@ if __name__ == '__main__':
         print('Please check the model selection args.')
         exit(0)
 
-    TrainModel(model, sys.argv[4], nn.CrossEntropyLoss(), int(sys.argv[5]), int(sys.argv[6]))
+    TrainModel(model, sys.argv[4], nn.CrossEntropyLoss(), int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]))
     print('All process done, cause %s seconds.' % (time.time() - start_time))
 
 
