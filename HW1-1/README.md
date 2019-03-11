8ec8309cea9c067e95fe75d3492ad3cce2249236
b05611038 <b05611038@ntu.edu.tw>
2019-03-11 15:56:51 +0800

    Finish HW1-1 coding

---
+++ HW1-1/README.md
@@ -0,0 +1,3 @@
+#HW1-1(Deep vs Shallow)
+
+To observe the performance of model with almost same amount of parameters but in differnet depth. 
---
+++ HW1-2/lib/model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ANN(nn.Module):
+    def __init__(self, depth, unit):
+        super(ANN, self).__init__()
+
+        self.depth = depth
+        self.unit = unit
+
+        self.denses = nn.Sequential()
+        self.denses.add_module('Dense_1', nn.Linear(1, unit, bias = False))
+        self.denses.add_module('ReLU_1', nn.ReLU())
+        for i in range(depth - 2):
+            self.denses.add_module('Dense_' + str(i + 2), nn.Linear(unit, unit, bias = False))
+            self.denses.add_module('ReLU_' + str(i + 2), nn.ReLU())
+
+        self.denses.add_module('Dense_' + str(depth), nn.Linear(unit, 1, bias = False))
+
+    def forward(self, data):
+        x = self.denses(data)
+
+        return x
+
+
