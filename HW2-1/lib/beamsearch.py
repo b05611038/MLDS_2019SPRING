80ef27252790c33f98dca908637e2782edaf623f
b05611038 <b05611038@ntu.edu.tw>
2019-04-07 19:12:16 +0800

    finish beam search

---
+++ HW2-1/lib/beamsearch.py
@@ -0,0 +1,101 @@
+import copy
+import torch
+import torch.cuda as cuda
+import torch.nn as nn
+import torch.nn.functional as F
+
+from lib.utils import *
+from lib.model import S2VT
+
+class BeamSearch(nn.Module):
+    def __init__(self, whole_model, word2vec, env, k = 2, max_length = 40):
+        super(BeamSearch, self).__init__()
+
+        self.whole_model = whole_model
+        self.env = env
+        self.word2vec = load_object(word2vec)
+        self.bos = torch.tensor(self.word2vec.w2v('<bos>'))
+        self.eos = torch.tensor(self.word2vec.w2v('<eos>'))
+        self.k = k
+        self.max_length = max_length
+        self.seq = None
+
+        self.encoder, decoder = self._load_trained_model(whole_model)
+        self.encoder.to(self.env)
+        self.lstm = decoder.lstm
+        self.embedding = decoder.embedding
+        self.embedding_dropout = decoder.embedding_dropout
+        self.cat = decoder.cat
+        self.linear = decoder.linear
+        self.attn = decoder.attn
+
+    def forward(self, video_seq):
+        e_h = torch.zeros(2, video_seq.size(1), self.encoder.hidden_size).to(self.env)
+        e_c = torch.zeros(2, video_seq.size(1), self.encoder.hidden_size).to(self.env)
+
+        video_embedding, (e_h, e_c) = self.encoder(video_seq, e_h, e_c)
+        d_c = torch.zeros(1, video_seq.size(1), self.encoder.hidden_size * 2).to(self.env)
+
+        self._beam_search(self.bos, video_embedding, d_c)
+        seq = copy.deepcopy(self.seq)
+        seq = self._biggest(seq)
+        self.seq = None
+        return seq
+
+    def _decode(self, video, input_token, h, c):
+        input_token = self.embedding(input_token)
+        input_token = self.embedding_dropout(input_token)
+
+        hiddens, (d_h, d_c) = self.lstm(input_token, (h, c))
+        attn_weight = self.attn(hiddens, video)
+        words_embedding = attn_weight.bmm(video.transpose(0, 1))
+        words_embedding = torch.cat((hiddens, input_token), dim = 2)
+        outs = words_embedding.view(-1, self.encoder.hidden_size * 4)
+        outs = torch.tanh(self.cat(outs))
+        outs = self.linear(outs)
+        outs = F.softmax(outs)
+
+        return outs, [d_h, d_c]
+
+    def _biggest(self, seq):
+        pro = 0
+        out = None
+        for i in range(len(seq)):
+            if (seq[i][1] > pro).item() == 1:
+                pro = seq[i][1].detach()
+                out = seq[i][0]
+
+        return out
+
+    def _beam_search(self, bos, video_embedding, c):
+        self.seq = []
+        bos = [torch.tensor([bos]), torch.tensor(1).float()]
+        self._k_search(bos, video_embedding, video_embedding[video_embedding.size(0) - 1:, :, :], c)
+        return None
+
+    def _k_search(self, past_seq, video_embedding, h, c):
+        if past_seq[0].size(0) >= self.max_length:
+            return None
+        last_word = past_seq[0][past_seq[0].size(0) - 1:]
+        last_word = last_word.view(1, 1)
+        new_out, hidden = self._decode(video_embedding, last_word, h, c)
+        pro, index = torch.topk(new_out, self.k)
+        pro = pro.detach().squeeze()
+        index = index.detach().squeeze()
+        for i in range(self.k):
+            if (index[i] == self.eos).item() == 1:
+                new_pro = past_seq[1] * pro[i]
+                seq = [torch.cat((past_seq[0], torch.tensor([index[i]])), dim = 0), new_pro]
+                self.seq.append(seq)
+                return None
+            else:
+                new_pro = past_seq[1] * pro[i]
+                seq = [torch.cat((past_seq[0], torch.tensor([index[i]])), dim = 0), new_pro]
+                self._k_search(seq, video_embedding, hidden[0], hidden[1])
+
+    def _load_trained_model(self, path):
+        s2vt = torch.load(path, map_location = 'cpu')
+        s2vt = s2vt.eval()
+        return s2vt.encoder, s2vt.decoder
+
+
---
+++ HW2-1/predict.py
@@ -0,0 +1,92 @@
+import os
+import sys
+import csv
+import time
+import numpy as np
+import torch
+
+from lib.utils import *
+from lib.beamsearch import BeamSearch
+
+def Een_setting(device):
+    if device < 0:
+        env = torch.device('cpu')
+        print('Envirnment setting done, using device: cpu')
+    else:
+        torch.backends.cudnn.benchmark = True
+        cuda.set_device(device)
+        env = torch.device('cuda:' + str(device))
+        print('Envirnment setting done, using device: CUDA_' + str(device))
+
+    return env
+
+def Grab_test(path):
+    for file in os.listdir(path):
+        test_set = []
+        if file.endswith('.npy'):
+            video = np.load(path + '/' + file)
+            test_set.append([video, file.replace('.npy', '')])
+
+    print('Testing set building done.')
+    return test_set
+
+def Predict(model, w2v, test_set, env, k):
+    searcher = BeamSearch(model, w2v, env, k = k)
+    w2v = load_object(w2v)
+    outcome = []
+    for i in range(len(test_set)):
+        video = test_set[i][0]
+        video = torch.tensor(video).view(80, 1, 4096).float().to(env)
+        seq = searcher(video).to('cpu')
+
+        no_title = True
+        sentence = test_set[i][1] + ','
+        for j in range(seq.size(0)):
+            word = w2v.v2w(torch.tensor([seq[j]]))
+            word, is_word = Clean(word)
+            if is_word and no_title:
+                word = word.title()
+                no_title = False
+
+            if is_word:
+                sentence = sentence + word + ' '
+            else:
+                pass
+
+        sentence += '\n'
+        outcome.append(sentence)
+        if i != 0 and i % 5 == 0:
+            print('Progress:', i + 1, '/', len(test_set))
+
+    return outcome
+
+def In_file(outcome):
+    f = open('output.txt', 'w')
+    f.writelines(outcome)
+    f.close()
+    print('Writing outcome to ./output.txt done.')
+
+def Clean(word):
+    word = word.replace('(', '')
+    word = word.replace(')', '')
+    print(word)
+    if word == '<eos>':
+        return '', False
+    elif word == '<bos>':
+        return '', False 
+    elif word == '<padding>':
+        return '', False
+    else:
+        return word, True
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('Usage: python3 predict.py [model] [word2vec] [search beam] [device]')
+        exit(0)
+
+    start_time = time.time()
+    env = Een_setting(int(sys.argv[4]))
+    test_set = Grab_test('./data/testing_data/feat')
+    outcome = Predict(sys.argv[1], sys.argv[2], test_set, env, int(sys.argv[3]))
+    In_file(outcome)
+    print('All process done, cause %s seconds.' % (time.time() - start_time))
